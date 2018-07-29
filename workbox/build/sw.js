1ac142c004213723904d3895010c6e55cd0b8076
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-07-29 22:56:12 +0800

    Added precaching of web assets.

--- workbox/build/sw.js
+++ workbox/build/sw.js
@@ -18,7 +18,28 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox
 
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
-    workbox.precaching.precacheAndRoute([]);
+    workbox.precaching.precacheAndRoute([
+  {
+    "url": "style/main.css",
+    "revision": "c7a02441b4914ffdc39eb2eb55148adc"
+  },
+  {
+    "url": "index.html",
+    "revision": "16dadfd966fd31a3013b34e6ac502d08"
+  },
+  {
+    "url": "js/animation.js",
+    "revision": "3f8fd475afa44c10b3107178a83bd9ae"
+  },
+  {
+    "url": "images/home/business.jpg",
+    "revision": "9c3ec8d2a8a188bab9ddc212a64a0c1e"
+  },
+  {
+    "url": "images/icon/icon.svg",
+    "revision": "d582b402cdafcc4a3934fba3986d1be7"
+  }
+]);
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
